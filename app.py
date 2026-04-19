f6ba520a0d0f0e4b47ba8f5abfbd8f03b89dd295
aayush8797823674 <aayush8797823674@gmail.com>
2026-04-19 16:31:28 +0530

    .

--- app.py
+++ app.py
@@ -6,7 +6,8 @@ from dotenv import load_dotenv
 
 # 1. Configuration & Setup
 load_dotenv()
-genai.configure(api_key=os.getenv("GEMINI_API_KEY"))
+# genai.configure(api_key=os.getenv("GEMINI_API_KEY"))
+genai.configure(api_key=st.secrets["GEMINI_API_KEY"])
 model = genai.GenerativeModel('gemini-flash-latest')
 
 # 2. Helper Function: Extract Text from PDF
