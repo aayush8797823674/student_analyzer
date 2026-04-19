71c44f539f192a260c8346edbafd4d59c9e011b1
aayush8797823674 <aayush8797823674@gmail.com>
2026-04-19 19:25:43 +0530

    .

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ import os
 from dotenv import load_dotenv
 
 # 1. Configuration & Setup
-load_dotenv()
+# load_dotenv()
 # genai.configure(api_key=os.getenv("GEMINI_API_KEY"))
 genai.configure(api_key=st.secrets["GEMINI_API_KEY"])
 model = genai.GenerativeModel('gemini-flash-latest')
